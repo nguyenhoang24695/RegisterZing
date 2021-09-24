0ef8ccff0fae9c089c34ed718476cd5fd4f632f5
hoangnv <hoangnvd00536@fpt.edu.vn>
2021-09-24 17:35:02 +0700

    update

--- RegisterZing/Program.cs
+++ RegisterZing/Program.cs
@@ -17,33 +17,44 @@ namespace RegisterZing
         {
             int id = 4;
 
-            var cService = ChromeDriverService.CreateDefaultService();
-            cService.HideCommandPromptWindow = true;
 
             // Đường dẫn tới thư mục chứa Chrome Driver
             // Tải về tại đây https://chromedriver.chromium.org/downloads
             // Thay đổi cho đúng với trường hợp của bạn
             string chromeDriverPath = @"./";
+            var cService = ChromeDriverService.CreateDefaultService(chromeDriverPath);
+            cService.HideCommandPromptWindow = true;
             var options = new ChromeOptions();
             options.AddExtensions("./mpbjkejclgfgadiemmefgebjfooflfhl.zip");
             options.AddArgument("no-sandbox");
+            options.AddArguments(new List<string>() { "disable-gpu" });
             // Chạy ngầm không pop up trình duyệt ra ngoài 
             //options.AddArgument("headless");
-            var driver = new ChromeDriver(cService, options, TimeSpan.FromDays(20));
+            //
+            IEnumerable<int> pidsBefore = Process.GetProcessesByName("chrome").Select(p => p.Id);
+            //
+            var driver = new ChromeDriver(cService, options);
+
+            //
+            IEnumerable<int> pidsAfter = Process.GetProcessesByName("chrome").Select(p => p.Id);
+            IEnumerable<int> newFirefoxPids = pidsAfter.Except(pidsBefore);
+
+            var b = newFirefoxPids.Where(p => Process.GetProcessById(p).MainWindowHandle != IntPtr.Zero).Select(p => Process.GetProcessById(p)).ToList();
+            Console.WriteLine(b.Count);
+            Console.WriteLine(b[0].Id);
+            //
             var _processId = cService.ProcessId;
             Console.WriteLine(_processId);
             Console.WriteLine(driver.CurrentWindowHandle);
-            var a = Process.GetProcessById(_processId);
 
-            IntPtr handle = a.Handle;
 
-            KAutoHelper.CaptureHelper.CaptureWindowToFile(handle,"./1.jpg", ImageFormat.Jpeg);
+            //KAutoHelper.CaptureHelper.CaptureScreenToFile("./1.jpg",ImageFormat.Jpeg);
 
             Console.ReadKey();
             string url = "https://id.zing.vn/";
             driver.Url = url;
 
-            
+
             //driver.Navigate().GoToUrl(url);
             driver.FindElement(By.XPath("/html/body/div[3]/div[2]/p[2]/a")).Click();
 
@@ -59,6 +70,24 @@ namespace RegisterZing
 
             var pass2 = driver.FindElement(By.XPath("/html/body/div[3]/div/div[2]/form/div[8]/input[2]"));
             pass2.SendKeys("liuyifei1@A");
+            //KAutoHelper.CaptureHelper.CaptureWindowToFile(b[0].MainWindowHandle, "./1.jpg", ImageFormat.Jpeg);
+            newFirefoxPids.ToList().ForEach(p =>
+            {
+                try
+                {
+
+                    var c = Process.GetProcessById(p);
+
+
+                    KAutoHelper.CaptureHelper.CaptureWindowToFile(c.MainWindowHandle, string.Format("./{0}.jpg", c.Id), ImageFormat.Jpeg);
+
+                }
+                catch
+                {
+
+                }
+
+            });
 
         }
     }
