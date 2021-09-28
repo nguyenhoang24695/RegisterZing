156d3a064f364b8f174a7dea2ed545039c7c24c4
hoangnv <hoangnvd00536@fpt.edu.vn>
2021-09-28 09:12:55 +0700

    update recapcha pass

--- RegisterZing/Program.cs
+++ RegisterZing/Program.cs
@@ -3,10 +3,12 @@ using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace RegisterZing
@@ -15,7 +17,10 @@ namespace RegisterZing
     {
         static void Main(string[] args)
         {
-            int id = 4;
+            var bm = new Bitmap("./pic/capcha.jpg");
+            var x = KAutoHelper.Get_Text_From_Image.Get_Text(bm);
+            Console.WriteLine("ID: ");
+            var id = int.Parse(Console.ReadLine());
 
 
             // Đường dẫn tới thư mục chứa Chrome Driver
@@ -28,67 +33,84 @@ namespace RegisterZing
             options.AddExtensions("./mpbjkejclgfgadiemmefgebjfooflfhl.zip");
             options.AddArgument("no-sandbox");
             options.AddArguments(new List<string>() { "disable-gpu" });
-            // Chạy ngầm không pop up trình duyệt ra ngoài 
-            //options.AddArgument("headless");
-            //
-            IEnumerable<int> pidsBefore = Process.GetProcessesByName("chrome").Select(p => p.Id);
-            //
-            var driver = new ChromeDriver(cService, options);
-
-            //
-            IEnumerable<int> pidsAfter = Process.GetProcessesByName("chrome").Select(p => p.Id);
-            IEnumerable<int> newFirefoxPids = pidsAfter.Except(pidsBefore);
-
-            var b = newFirefoxPids.Where(p => Process.GetProcessById(p).MainWindowHandle != IntPtr.Zero).Select(p => Process.GetProcessById(p)).ToList();
-            Console.WriteLine(b.Count);
-            Console.WriteLine(b[0].Id);
-            //
-            var _processId = cService.ProcessId;
-            Console.WriteLine(_processId);
-            Console.WriteLine(driver.CurrentWindowHandle);
-
-
-            //KAutoHelper.CaptureHelper.CaptureScreenToFile("./1.jpg",ImageFormat.Jpeg);
-
-            Console.ReadKey();
-            string url = "https://id.zing.vn/";
-            driver.Url = url;
-
-
-            //driver.Navigate().GoToUrl(url);
-            driver.FindElement(By.XPath("/html/body/div[3]/div[2]/p[2]/a")).Click();
-
-
-            var name = driver.FindElement(By.XPath("/html/body/div[3]/div/div[2]/form/div[2]/input"));
-            name.SendKeys("nhan nguyen");
-
-            var user = driver.FindElement(By.XPath("/html/body/div[3]/div/div[2]/form/div[4]/input"));
-            user.SendKeys(string.Format("nhatmong95_{0}", id));
-
-            var pass = driver.FindElement(By.XPath("/html/body/div[3]/div/div[2]/form/div[6]/input[2]"));
-            pass.SendKeys("liuyifei1@A");
-
-            var pass2 = driver.FindElement(By.XPath("/html/body/div[3]/div/div[2]/form/div[8]/input[2]"));
-            pass2.SendKeys("liuyifei1@A");
-            //KAutoHelper.CaptureHelper.CaptureWindowToFile(b[0].MainWindowHandle, "./1.jpg", ImageFormat.Jpeg);
-            newFirefoxPids.ToList().ForEach(p =>
+            while (id < 10)
             {
-                try
+                Console.WriteLine("Register with id =  " + id);
+                // Chạy ngầm không pop up trình duyệt ra ngoài 
+                //options.AddArgument("headless");
+                //
+                IEnumerable<int> pidsBefore = Process.GetProcessesByName("chrome").Select(p => p.Id);
+                //
+                var driver = new ChromeDriver(cService, options);
+
+                //
+                IEnumerable<int> pidsAfter = Process.GetProcessesByName("chrome").Select(p => p.Id);
+                IEnumerable<int> newFirefoxPids = pidsAfter.Except(pidsBefore);
+
+                var b = newFirefoxPids.Where(p => Process.GetProcessById(p).MainWindowHandle != IntPtr.Zero).Select(p => Process.GetProcessById(p)).ToList();
+                Console.WriteLine(b[0].Id);
+
+                //KAutoHelper.CaptureHelper.CaptureScreenToFile("./1.jpg",ImageFormat.Jpeg);
+
+                string url = "https://id.zing.vn/";
+                driver.Url = url;
+
+
+                //driver.Navigate().GoToUrl(url);
+                driver.FindElement(By.XPath("/html/body/div[3]/div[2]/p[2]/a")).Click();
+
+
+                var name = driver.FindElement(By.XPath("/html/body/div[3]/div/div[2]/form/div[2]/input"));
+                name.SendKeys("nhan nguyen");
+
+                var user = driver.FindElement(By.XPath("/html/body/div[3]/div/div[2]/form/div[4]/input"));
+                user.SendKeys(string.Format("nhatmong95_{0}", id));
+
+                var pass = driver.FindElement(By.XPath("/html/body/div[3]/div/div[2]/form/div[6]/input[2]"));
+                pass.SendKeys("liuyifei1@A");
+
+                var pass2 = driver.FindElement(By.XPath("/html/body/div[3]/div/div[2]/form/div[8]/input[2]"));
+                pass2.SendKeys("liuyifei1@A");
+                KAutoHelper.CaptureHelper.CaptureWindowToFile(b[0].MainWindowHandle, "./1.jpg", ImageFormat.Jpeg);
+                //driver.FindElement(By.XPath("/html/body/div[3]/div/div[2]/form/div[15]/a")).Click();
+                var mainBitmap = KAutoHelper.CaptureHelper.CaptureWindow(b[0].MainWindowHandle);
+                var point = KAutoHelper.ImageScanOpenCV.FindOutPoint((Bitmap)mainBitmap, new Bitmap(string.Format("./pic/register_Btn.jpg")));
+                if (point.HasValue)
                 {
-
-                    var c = Process.GetProcessById(p);
-
-
-                    KAutoHelper.CaptureHelper.CaptureWindowToFile(c.MainWindowHandle, string.Format("./{0}.jpg", c.Id), ImageFormat.Jpeg);
-
+                    KAutoHelper.AutoControl.SendClickOnPosition(b[0].MainWindowHandle, point.Value.X, point.Value.Y);
                 }
-                catch
+                //KAutoHelper.CaptureHelper.CaptureWindowToFile(b[0].MainWindowHandle, "./1.jpg", ImageFormat.Jpeg);
+
+                Thread.Sleep(1000);
+                int count = 0;
+                while (count < 3)
                 {
+                    Console.WriteLine("Find count: " + (count + 1));
+                    try
+                    {
+                        mainBitmap = KAutoHelper.CaptureHelper.CaptureWindow(b[0].MainWindowHandle);
 
+                        point = KAutoHelper.ImageScanOpenCV.FindOutPoint((Bitmap)mainBitmap, new Bitmap(string.Format("./pic/20908.jpg")));
+                        if (point.HasValue)
+                        {
+                            KAutoHelper.AutoControl.SendClickOnPosition(b[0].MainWindowHandle, point.Value.X, point.Value.Y);
+                        }
+                        else
+                        {
+                            count++;
+                        }
+                        Thread.Sleep(2000);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        break;
+                    }
                 }
 
-            });
-
+                driver.Dispose();
+                id++;
+            }
         }
     }
 }
